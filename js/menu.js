dfac8231f0ffb2eb4f3a27def684f9b230bfa1c6
Bart0llo <bartekn041@gmail.com>
2023-01-15 19:27:50 +0100

    format

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,9 @@
-
 function Menu() {
-    const x = document.getElementById('links');
+  const x = document.getElementById("links");
 
-    if (x.style.display === "flex") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "flex";
-      }
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
 }
